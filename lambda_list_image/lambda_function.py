c1ce3e88b7cf98592e4057d4784699034cd504a5
Ella-zizzzy <zl3253@nyu.edu>
2024-12-01 15:25:55 -0600

    add lambda functions: users, listimage, upload

---
+++ lambda_list_image/lambda_function.py
@@ -0,0 +1,115 @@
+#
+# Retrieves and returns a user's all the images' info in the 
+# PixelTailor database.
+#
+
+import json
+import boto3
+import os
+import datatier
+
+from configparser import ConfigParser
+
+def lambda_handler(event, context):
+  try:
+    print("**STARTING**")
+    print("**lambda: final_users**")
+    
+    #
+    # setup AWS based on config file:
+    #
+    #TODO: config file needed
+    #
+    config_file = '####'
+    os.environ['AWS_SHARED_CREDENTIALS_FILE'] = config_file
+    
+    configur = ConfigParser()
+    configur.read(config_file)
+    
+    #
+    # configure for RDS access
+    #
+    rds_endpoint = configur.get('rds', 'endpoint')
+    rds_portnum = int(configur.get('rds', 'port_number'))
+    rds_username = configur.get('rds', 'user_name')
+    rds_pwd = configur.get('rds', 'user_pwd')
+    rds_dbname = configur.get('rds', 'db_name')
+
+    # userid from event: could be a parameter
+    # or could be part of URL path ("pathParameters"):
+    #
+    print("**Accessing event/pathParameters**")
+    
+    if "userid" in event:
+      userid = event["userid"]
+    elif "pathParameters" in event:
+      if "userid" in event["pathParameters"]:
+        userid = event["pathParameters"]["userid"]
+      else:
+        raise Exception("requires userid parameter in pathParameters")
+    else:
+        raise Exception("requires userid parameter in event")
+        
+    print("userid:", userid)
+    
+
+    #
+    # open connection to the database:
+    #
+    print("**Opening connection**")
+    
+    dbConn = datatier.get_dbConn(rds_endpoint, rds_portnum, rds_username, rds_pwd, rds_dbname)
+
+    #
+    # first we need to make sure the userid is valid:
+    #
+    print("**Checking if userid is valid**")
+    
+    sql1 = "SELECT * FROM users WHERE userid = %s;"
+    
+    row1 = datatier.retrieve_one_row(dbConn, sql1, [userid])
+    
+    if row1 == ():  # no such user
+      print("**No such user, returning...**")
+      return {
+        'statusCode': 400,
+        'body': json.dumps("no such user...")
+      }
+    
+    print(row1)
+
+    #
+    # now retrieve images:
+    #
+    print("**Retrieving data**")
+
+    #
+    # select the users' images from the 
+    # photos table, ordered by photoid
+    #
+    sql2 = "SELECT photoid, userid, original_name FROM photos WHERE userid=%s ORDER BY photoid";
+    
+    rows = datatier.retrieve_all_rows(dbConn, sql2, [userid])
+    
+    for row in rows:
+      print(row)
+
+    #
+    # respond in an HTTP-like way, i.e. with a status
+    # code and body in JSON format:
+    #
+    print("**DONE, returning rows**")
+    
+    return {
+      'statusCode': 200,
+      'body': json.dumps(rows)
+    }
+    
+  except Exception as err:
+    print("**ERROR**")
+    print(str(err))
+    
+    return {
+      'statusCode': 500,
+      'body': json.dumps(str(err))
+    }
